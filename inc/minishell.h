3765f3b57a0bd367e857fb6f3f3eb10df89ba467
charles <charles.nikdel@gmail.com>
2023-06-16 13:33:14 +0200

    header je pense

---
+++ inc/minishell.h
@@ -0,0 +1,24 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <termios.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <string.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <signal.h>
+# include <term.h>
+# include <curses.h>
+# include <limits.h>
+
+#endif
